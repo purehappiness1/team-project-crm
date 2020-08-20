96e1d14e35bcbbcb2fc988fae67d5e28619f795b
Maria <maneysupra@rambler.ru>
2020-08-20 18:42:02 +0300

    Project

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: String,
+  status: { type: Number, default: 1 },  // 1 - manager, 2 - admin
+  date: Date,
+  scrf_token: {
+    type: String
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
